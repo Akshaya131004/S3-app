64da4493c7c704c6a73005c01d6ae0da0fadfbdb
Akshaya131004 <akshayad617@gmail.com>
2025-11-18 01:04:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_TMOrBEgtL', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3avkuna5itq28rgag3l7lcur15', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_ATrbDgSt3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7e0agalkucnmrji586bid551u6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://k729q1eo87.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ub1jqf9qe7.execute-api.ap-south-1.amazonaws.com/S3-pipeline' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
